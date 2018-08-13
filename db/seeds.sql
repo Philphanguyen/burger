600a980099101b0b6efdbee64ee1dfb430a06cdb
Pip1242 <philphanguyen@gmail.com>
2018-08-13 16:00:49 -0400

    adding files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Royale with Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Burger 1492');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Garlic Mushroom Melt', true);
